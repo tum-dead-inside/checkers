299b2bae5cf3ad38ab321a5b3667444a3ef7452d
justchokingaround <ivanonarch@tutanota.com>
2024-02-04 15:19:05 +0100

    chore: update shell.nix and requirements.txt

--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
+
 let
-  my-python-packages = ps: with ps; [
-    tkinter
-    pillow
-  ];
+  my-python-packages = ps: with ps; [ tkinter pillow ];
   my-python = pkgs.python3.withPackages my-python-packages;
-in my-python.env
+  pyright = pkgs.nodePackages.pyright;
+  pipreqs = pkgs.pipreqs;
+in
+pkgs.mkShell {
+  buildInputs = [ my-python pyright pipreqs ];
+}
